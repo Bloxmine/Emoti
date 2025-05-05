0beb4e1fd247c0b7c762589ebdc75e1825eb3d5b
Horik <heindijstelbloem@hotmail.com>
2025-05-05 19:38:32 +0200

    Added all emoticon

--- js/main.js
+++ js/main.js
@@ -6,43 +6,54 @@ const emoticonData = [
             { id: '16', code: ':tramp:', url: '../emoticons/hyves/tramp.gif' },
             { id: '17', code: ':talktothehand:', url: '../emoticons/hyves/talktothehand.gif' },
             { id: '18', code: ':wk2010:', url: '../emoticons/hyves/wk2010.gif' },
-            { id: '19', code: ':custom4:', url: 'https://via.placeholder.com/32x32.gif?text=Custom4' },
-            { id: '20', code: ':custom5:', url: 'https://via.placeholder.com/32x32.gif?text=Custom5' },
+            { id: '19', code: ':dj:', url: '../emoticons/hyves/dj.gif' },
+            { id: '20', code: ':lekkerding:', url: '../emoticons/hyves/lekkerding.gif' },
         ],
     },
     {
         name: ' MSN Emoticons',
         iconUrl: '../icons/msn.png', // Path to the PNG icon
         emoticons: [
-            { id: '1', code: ':)', url: 'https://via.placeholder.com/32x32.gif?text=MSN1', alias: ['happy'] },
-            { id: '2', code: ':(', url: 'https://via.placeholder.com/32x32.gif?text=MSN2', alias: ['sad'] },
-            { id: '3', code: ':D', url: 'https://via.placeholder.com/32x32.gif?text=MSN3', alias: ['bigsmile', 'grin'] },
-            { id: '4', code: ';)', url: 'https://via.placeholder.com/32x32.gif?text=MSN4', alias: ['wink'] },
-            { id: '5', code: '(h)', url: 'https://via.placeholder.com/32x32.gif?text=MSN5', alias: ['heart'] },
+            { id: '1', code: ':brb:', url: '../emoticons/msn/brb.gif', alias: ['brb'] },
+            { id: '2', code: ':dog:', url: '../emoticons/msn/dog.png', alias: ['dog'] },
+            { id: '3', code: ':laugh:', url: '../emoticons/msn/laugh.png', alias: ['laugh'] },
+            { id: '4', code: ':wink:', url: '../emoticons/msn/wink.gif', alias: ['wink'] },
+            { id: '5', code: ':nerd:', url: '../emoticons/msn/nerd.gif', alias: ['nerd'] },
         ],
     },
     {
         name: ' Yahoo Emoticons',
         iconUrl: '../icons/yahoo.png', // Path to the PNG icon
         emoticons: [
-            { id: '6', code: ':-)', url: 'https://via.placeholder.com/32x32.gif?text=Yahoo1', alias: ['smile'] },
-            { id: '7', code: ':-(', url: 'https://via.placeholder.com/32x32.gif?text=Yahoo2', alias: ['frown'] },
-            { id: '8', code: ':>', url: 'https://via.placeholder.com/32x32.gif?text=Yahoo3' },
-            { id: '9', code: ';-)', url: 'https://via.placeholder.com/32x32.gif?text=Yahoo4' },
-            { id: '10', code: ':x', url: 'https://via.placeholder.com/32x32.gif?text=Yahoo5' },
+            { id: '6', code: ':alien:', url: '../emoticons/yahoo/alien.gif', alias: ['alien'] },
+            { id: '7', code: ':bringit:', url: '../emoticons/yahoo/bringit.gif', alias: ['bringiton'] },
+            { id: '8', code: ':loser:', url: '../emoticons/yahoo/loser.gif' },
+            { id: '9', code: ':rofl:', url: '../emoticons/yahoo/rofl.gif' },
+            { id: '10', code: ':star:', url: '../emoticons/yahoo/star.gif' },
         ],
     },
     {
         name: ' phpBB Emoticons',
         iconUrl: '../icons/phpbb.png', // Path to the PNG icon
         emoticons: [
-            { id: '11', code: ':mrgreen:', url: 'https://via.placeholder.com/32x32.gif?text=phpBB1' },
-            { id: '12', code: ':|', url: 'https://via.placeholder.com/32x32.gif?text=phpBB2' },
-            { id: '13', code: ':lol:', url: 'https://via.placeholder.com/32x32.gif?text=phpBB3' },
-            { id: '14', code: ':o', url: 'https://via.placeholder.com/32x32.gif?text=phpBB4' },
-            { id: '15', code: ':p', url: 'https://via.placeholder.com/32x32.gif?text=phpBB5' },
+            { id: '11', code: ':applause:', url: '../emoticons/phpbb/applause.gif', alias: ['applause'] },
+            { id: '12', code: ':eek:', url: '../emoticons/phpbb/eek.gif', alias: ['eek'] },
+            { id: '13', code: ':exclaim:', url: '../emoticons/phpbb/exclaim.gif', alias: ['exclaim'] },
+            { id: '14', code: ':mrgeen:', url: '../emoticons/phpbb/mrgreen.gif', alias: ['mrgreen'] },
+            { id: '15', code: ':twisted:', url: '../emoticons/phpbb/twisted.gif', alias: ['twisted'] },
         ],
-    }
+    },
+    {
+        name: ' Skype Emoticons',
+        iconUrl: '../icons/skype.png', // Path to the PNG icon
+        emoticons: [
+            { id: '21', code: ':dance:', url: '../emoticons/skype/dance.gif' },
+            { id: '22', code: ':emo:', url: '../emoticons/skype/emo.gif' },
+            { id: '23', code: ':finger:', url: '../emoticons/skype/finger.gif' },
+            { id: '24', code: ':party:', url: '../emoticons/skype/party.gif' },
+            { id: '25', code: ':tmi:', url: '../emoticons/skype/tmi.gif' },
+        ],
+    },
 ];
 
 window.onload = function() {
